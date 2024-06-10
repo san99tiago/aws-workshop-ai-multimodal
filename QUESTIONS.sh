da7659d3cd0b238aaecaf3b8d365964230ee6df5
Santiago Garcia Arango <san99tiago@gmail.com>
2024-06-09 21:09:32 -0500

    Add documentation for UG Med event

---
+++ QUESTIONS.sh
@@ -0,0 +1,19 @@
+# PREGUNTAS
+
+# 1) Cómo se maneja el tema de pruebas en estos modelos?
+
+# 2) Cómo se recomienda el manejo de throttling en estos modelos?
+
+# 3) Explicar cuándo usar RAG vs Fine-Tuning?
+
+# 4) Cómo agregar nuestros propios datos/bases-de-datos a estos modelos?
+
+# 5) Cómo se maneja el tema de la privacidad en estos modelos?
+
+# 7) En qué momentos recomiendas entrenar nuestros propios modelos, en vez de usar LLMs con prompts?
+
+# 8( ) decirle  al modelo que es un experto y decirle "que se va a ganar un bono por hacer un buen trabajo" o algo asi... Sencillo pero sirve...
+
+# 9) Profundizar entre Amazon Bedrock Agents vs LangChain Tools
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# GENERAL WORKSHOP OF AI MULTIMODAL SOLUTIONS WITH AWS
+
+## AWS User Group Medellin (2024-06-07)
+
+Official AWS Workshop:
+
+- https://catalog.workshops.aws/building-with-amazon-bedrock/en-US
+
+This is just some of the sample code used during the workshop. All of it is inspired/developed by other open source projects.
